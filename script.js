4c00922bf950309a5d292bc7433d100663aeeed3
danielpiecyk <100698293+DanielPiecyk@users.noreply.github.com>
2022-05-24 12:45:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+(function () {
+  const ex1_button = document.getElementById('ex1_button')
+})();
+  ex1_button.addEventListener("click", function () {
+    var cw1 = "0,1,2,3,4,5,6,7,8,9";
+    ex1_content.innerHTML = cw1 ;
+  })
